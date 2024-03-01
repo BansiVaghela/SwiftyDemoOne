5d5c6dfb8d444b05fbbbe823c3b818da8c657445
Bansi Vaghela <bansi.vaghela@volansys.com>
2024-03-01 12:09:53 +0530

    Initial project setup

---
+++ SwiftyDemoOne/SwiftyDemoOne.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyDemoOne.h
+//  SwiftyDemoOne
+//
+//  Created by Bansi Vaghela on 01/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyDemoOne.
+FOUNDATION_EXPORT double SwiftyDemoOneVersionNumber;
+
+//! Project version string for SwiftyDemoOne.
+FOUNDATION_EXPORT const unsigned char SwiftyDemoOneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyDemoOne/PublicHeader.h>
+
+
---
+++ SwiftyDemoOneExample/SwiftyDemoOneExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyDemoOneExampleApp.swift
+//  SwiftyDemoOneExample
+//
+//  Created by Bansi Vaghela on 01/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyDemoOneExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
